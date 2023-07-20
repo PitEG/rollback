22ed73b6bd36af31c4471feb8833bd27a22bedb9
Enrique Gan <enriqueganpit@gmail.com>
2023-07-19 18:07:03 -0700

    check

--- include/session.hpp
+++ include/session.hpp
@@ -1,6 +1,9 @@
 #pragma once
+#include "player.hpp"
+#include <vector>
 
 namespace rollback {
   class Session {
+    Session(const std::vector<Player> players = {});
   };
 }
--- include/state.hpp
+++ include/state.hpp
@@ -1,8 +1,12 @@
 #pragma once
+#include "input.hpp"
 
 namespace rollback {
   template<class T> 
   class State {
     T state;
+
+    template<class R>
+    void Update(const Input<R>& input);
   };
 }
